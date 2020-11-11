feefaf031d2763ad7eb87206f0011405f939636b
Sean Njenga <seanisaacnjenga@gmail.com>
2020-11-11 09:00:09 -0600

    Added CSV Functionality to RS Scrape
    Instead of printing to terminal it now prints to a csv file named 'results.csv'

--- MangaPriceScraping/RightStufAnimeScrap.py
+++ MangaPriceScraping/RightStufAnimeScrap.py
@@ -1,12 +1,14 @@
+#Scraps the RightStufAnime website for a manga or light novel series and prints the stock stats, title, and current price
+
 from msedge.selenium_tools import Edge, EdgeOptions
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 import csv
 import json
 import time
-
-#Scraps the RightStufAnime website for a manga or light novel series and prints the stock stats, title, and current price
 
 def filterTitle(title):
     #title = title.replace(" ", "")
@@ -14,26 +16,29 @@ def filterTitle(title):
         if char in title:
             title = title.replace(char, "%" + hex(ord(char))).replace("0x", "")
     return title
-            
+
+#Gets the URL for the Manga the user wants   
 def getMangaURL(mangaTitle):
     mangaTitle = filterTitle(mangaTitle)
-    mangaURL = R"https://www.rightstufanime.com/category/Manga?order=custitem_rs_release_date:asc&show=96&keywords={}".format(mangaTitle)
-    # print(mangaURL)
+    mangaURL = R"https://www.rightstufanime.com/category/Manga?page=1&order=custitem_rs_release_date:asc&show=96&keywords={}".format(mangaTitle)
+    print(mangaURL)
     return mangaURL
 
+#Gets the URL to for the Light Novel the user wants
 def getLightNovelURL(lightNovelTitle):
     lightNovelTitle = filterTitle(lightNovelTitle)
-    lightNovelURL = R"https://www.rightstufanime.com/category/Novels?order=custitem_rs_release_date:asc&show=96&keywords={}".format(lightNovelTitle)
-    # print(lightNovelURL)
+    lightNovelURL = R"https://www.rightstufanime.com/category/Novels?page=1&order=custitem_rs_release_date:asc&show=96&keywords={}".format(lightNovelTitle)
+    print(lightNovelURL)
     return lightNovelURL
 
+# def pagination():
+#     page = driver.find_element_by_link_text
 
+    
 def getRSPrice(title, bookType):
     options = EdgeOptions()
     options.use_chromium = True
     driver = Edge(options=options)
-    
-    #print("BookType Test: " + bookType + "\n Title Test: " + title)
     
     #Ask the user whether they are looking for a LightNovel or Manga
     if bookType == "M": #Checks if the booktype is a manga
@@ -43,23 +48,26 @@ def getRSPrice(title, bookType):
     else:
         print("Invalid Book Type, must be a Manga (M) or Light Novel (LN)")
         return
+
+    driver.get(url)
+    time.sleep(5)
     
     #Parse the HTML to start scraping for data
-    print(url)
-    driver.get(url)
-    time.sleep(10) #Takes a little whiel to load the page so need to wait until it's done loading before scraping
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     
     #Get the Title, Price, and Stock Status Data of each Manga Volume
-    titleData = soup.find_all('span', {'itemprop' : 'name'})
-    priceData = soup.find_all('span', {'itemprop' : 'price'})
-    stockStatusData = soup.find_all('div', {'class' : 'product-line-stock-container'})
+    titles = soup.find_all('span', {'itemprop' : 'name'})
+    prices = soup.find_all('span', {'itemprop' : 'price'})
+    stockStatus = soup.find_all('div', {'class' : 'product-line-stock-container'})
     #print(titleData)
     
-    for stockStatus, fullTitle, price in zip(stockStatusData, titleData, priceData): #get only the title and volume number for the series we are looking for
-        if title in fullTitle.text: #Only get the data for the series the user wants
-            print(stockStatus.text + "  " + fullTitle.text + "  " + price.text)
-    
+    with open ("results.csv", "w", newline = "", encoding = "utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Title", "Stock Status", "Price"])
+        for stockStatus, fullTitle, price in zip(stockStatus, titles, prices): #get only the title and volume number for the series we are looking for
+            if title in fullTitle.text: #Only get the data for the series the user wants
+                writer.writerow([fullTitle.text, stockStatus.text, price.text])
+            
 
 #Get the book type and title from the user to find data for      
 userBookType = input("Enter Book Type (LN or M): ")
