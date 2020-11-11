4dfe87044da7f0e025f4a7b580862fcefb9cf826
Sean Njenga <seanisaacnjenga@gmail.com>
2020-11-10 19:39:05 -0600

    RightStufAnime Scrap Additions
    Fixed issue where the url is valid but no data is scraped due to the soap scrape method being called before the webpage is fully loaded. Added functionality that asks the user whether they want the data for a manga or a light novel and then the title of the series. Cleaned up some stuff and added comments

--- MangaPriceScraping/RightStufAnimeScrap.py
+++ MangaPriceScraping/RightStufAnimeScrap.py
@@ -1,43 +1,67 @@
 from msedge.selenium_tools import Edge, EdgeOptions
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
+from tabulate import tabulate
 import csv
-import re
-import codecs
+import json
+import time
 
-#Generate RightStufAnime URL based on the title of the manga
+#Scraps the RightStufAnime website for a manga or light novel series and prints the stock stats, title, and current price
 
-def filterMangaTitle(mangaTitle):
-    mangaTitle = mangaTitle.replace(" ", "")
-    for char in ['\'', '!']:
-        if char in mangaTitle:
-            mangaTitle = mangaTitle.replace(char, "%" + hex(ord(char))).replace("0x", "")
-    return mangaTitle
+def filterTitle(title):
+    #title = title.replace(" ", "")
+    for char in ["'", "!", " "]:
+        if char in title:
+            title = title.replace(char, "%" + hex(ord(char))).replace("0x", "")
+    return title
             
 def getMangaURL(mangaTitle):
-    mangaTitle = filterMangaTitle(mangaTitle)
-    url = R"https://www.rightstufanime.com/category/Manga?order=custitem_rs_release_date:asc&show=96&keywords={}".format(mangaTitle)
-    print(url)
-    return url
+    mangaTitle = filterTitle(mangaTitle)
+    mangaURL = R"https://www.rightstufanime.com/category/Manga?order=custitem_rs_release_date:asc&show=96&keywords={}".format(mangaTitle)
+    # print(mangaURL)
+    return mangaURL
+
+def getLightNovelURL(lightNovelTitle):
+    lightNovelTitle = filterTitle(lightNovelTitle)
+    lightNovelURL = R"https://www.rightstufanime.com/category/Novels?order=custitem_rs_release_date:asc&show=96&keywords={}".format(lightNovelTitle)
+    # print(lightNovelURL)
+    return lightNovelURL
 
 
-def getRSPrice(mangaTitle):
-    #Hard-coded path
-    #PATH = "Some Path"
+def getRSPrice(title, bookType):
     options = EdgeOptions()
     options.use_chromium = True
     driver = Edge(options=options)
     
-    driver.get(getMangaURL(mangaTitle))
+    #print("BookType Test: " + bookType + "\n Title Test: " + title)
     
+    #Ask the user whether they are looking for a LightNovel or Manga
+    if bookType == "M": #Checks if the booktype is a manga
+        url = getMangaURL(title)
+    elif bookType == "LN": #Checks if the book type is a Light Novel
+        url = getLightNovelURL(title)
+    else:
+        print("Invalid Book Type, must be a Manga (M) or Light Novel (LN)")
+        return
+    
+    #Parse the HTML to start scraping for data
+    print(url)
+    driver.get(url)
+    time.sleep(10) #Takes a little whiel to load the page so need to wait until it's done loading before scraping
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     
-    #Get the Title and Price Data of each Manga Volume
+    #Get the Title, Price, and Stock Status Data of each Manga Volume
     titleData = soup.find_all('span', {'itemprop' : 'name'})
     priceData = soup.find_all('span', {'itemprop' : 'price'})
+    stockStatusData = soup.find_all('div', {'class' : 'product-line-stock-container'})
+    #print(titleData)
     
-    for title, price in zip(titleData, priceData): #get only the title and volume number for the series we are looking for
-        if mangaTitle.lower() in title.text.lower(): #Check to see if the series's title is present
-            print(title.text + '    ' + price.text)
-            
-getRSPrice(input("Enter Manga Title: "))
+    for stockStatus, fullTitle, price in zip(stockStatusData, titleData, priceData): #get only the title and volume number for the series we are looking for
+        if title in fullTitle.text: #Only get the data for the series the user wants
+            print(stockStatus.text + "  " + fullTitle.text + "  " + price.text)
+    
+
+#Get the book type and title from the user to find data for      
+userBookType = input("Enter Book Type (LN or M): ")
+userBookTitle = input("Enter Manga Title: " if userBookType == "M" else "Enter Light Novel Title: ")          
+getRSPrice(userBookTitle, userBookType)
